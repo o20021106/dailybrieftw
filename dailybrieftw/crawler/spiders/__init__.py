5426a93f333a91c9c969eb87f582afceac2ba596
iping <iping@ailabs.tw>
2021-01-14 17:57:55 +0800

    [feature] scrapy spiders: appledaily, ltn, udn, chinatimes

---
+++ dailybrieftw/crawler/spiders/__init__.py
@@ -0,0 +1,6 @@
+from .appledaily import AppleDailySpider
+from .chinatimes import ChinaTimesSpider
+from .ltn import LtnSpider
+from .udn import UdnSpider
+
+__all__ = ['AppleDailySpider', 'ChinaTimesSpider', 'LtnSpider', 'UdnSpider']
